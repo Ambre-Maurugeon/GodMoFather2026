9c866075cd321fb192de2271c46ea68809b07827
PokMonOOO <zlaurent0@gmail.com>
2026-09-11 15:05:43 +0200

    [FIX] Angry meter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -121,7 +121,7 @@ public class GameManager : MonoBehaviour
     public void IncrementAngryMeter()
     {
         _monsterAngryMeter++;
-
+        monsterAngryMeter.SetMeter(this._monsterAngryMeter);
     }
     void ChangePlayerTurn(int changeTo)
     {
